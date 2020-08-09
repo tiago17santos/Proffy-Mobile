e41b6c4f02215e8a324f8cdead67818ac65c5494
tiago17santos <67645178+tiago17santos@users.noreply.github.com>
2020-08-09 04:44:01 -0300

    Setup and Layout inital

---
+++ src/routes/StudyTabs.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from "@expo/vector-icons";
+
+import TeacheraList from '../pages/TeacherList';
+import Favorites from '../pages/Favorites';
+
+
+const { Navigator, Screen } = createBottomTabNavigator()
+
+function StudyTabs(){
+    return (
+        <Navigator
+        tabBarOptions={{
+            style: {
+                height: 64,
+                elevation: 0,
+                shadowOpacity: 0,
+            },
+            tabStyle: {
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+            },
+            iconStyle: {
+                flex: 0,
+                width: 20,
+                height: 20,
+            },
+            labelStyle: {
+                marginLeft: 16,
+                fontFamily: "Archivo_700Bold",
+                fontSize: 13,
+            },
+            inactiveBackgroundColor: "#fafafc",
+            activeBackgroundColor: "#ebebf5",
+            inactiveTintColor: "#c1bccc",
+            activeTintColor: "#32264b",
+        }}
+        >
+            <Screen 
+            name="TeacherList" 
+            component={TeacheraList} 
+            options={{
+                tabBarIcon: ({ color, focused, size }) => (
+                    <Ionicons
+                        color={focused ? "#8257e5" : color}
+                        name="ios-easel"
+                        size={size}
+                    />
+                ),
+                tabBarLabel: "Proffys",
+            }}
+            />
+            <Screen 
+            name="Favorite" 
+            component={Favorites} 
+            options={{
+                tabBarIcon: ({ color, focused, size }) => (
+                    <Ionicons
+                        color={focused ? "#8257e5" : color}
+                        name="ios-heart"
+                        size={size}
+                    />
+                ),
+                tabBarLabel: "Favoritos",
+            }}
+            />
+        </Navigator>
+    )
+}
+
+export default StudyTabs;
